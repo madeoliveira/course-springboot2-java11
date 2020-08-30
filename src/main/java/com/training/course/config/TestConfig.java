541556920b52a46ddb2303eb2dab21c59ff45fd7
Marcio Oliveira <marcio.alves78@hotmail.com>
2020-08-30 20:37:43 -0300

    Many-to-Many association with JoinTable.

--- src/main/java/com/training/course/config/TestConfig.java
+++ src/main/java/com/training/course/config/TestConfig.java
@@ -51,6 +51,16 @@ public class TestConfig implements CommandLineRunner{
 		
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);	
+		
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
 		User u1 = new User(null,"Rafeal Franco","rafael@gmail.com", "90999999", "12121212");
 		User u2 = new User(null, "Marcio Alves","marcio@gmail.com","63636363","32323232");
 		
